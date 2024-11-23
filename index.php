c553611578aff7330797c80d95d6a439cd13cf54
Shahed <shahed_hridoy@outlook.com>
2024-11-23 22:51:33 +0600

    file upload

--- index.php
+++ index.php
@@ -9,12 +9,8 @@
 <body>
     <h2>Upload Student Photo</h2>
     <form action="upload.php" method="post" enctype="multipart/form-data">
-        <label for="studentName">Student Name:</label>
-        <input type="text" name="studentName" id="studentName" required><br><br>
-
         <label for="photo">Student Photo:</label>
-        <input type="file" name="fileToUpload" id="fileToUpload">
-<!--         <input type="file" name="photo" id="photo" accept="image/*" required><br><br> -->
+        <input type="file" name="fileToUpload" accept="image/*" id="photo" required />
         <input type="submit" name="submit" value="Upload Image">
     </form>
 </body>
